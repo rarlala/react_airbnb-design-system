9e03cae10d0c09e4dd6284031f0c70c425207e3b
rarlala <wtw9611@gmail.com>
2020-06-18 16:49:30 +0900

    code commit: stylecomponent 작성 부분까지 commit

---
+++ src/sample/Theme.js
@@ -0,0 +1,28 @@
+export default {
+    color: {
+        primary: '#03a9f4',
+        secondary: '#795548',
+        white: '#ffffff',
+        gray: '#cccccc',
+        default: '#999999'
+    },
+
+    size: {
+        xg: 24,
+        lg: 18,
+        md: 14,
+        sm: 12,
+        xs: 10
+    },
+
+    lineHeight: {
+        xg: '60px',
+        lg: '54px',
+        md: '36px',
+        sm: '24px',
+        xs: '18px',
+    },
+
+    unit: 4,
+
+}
